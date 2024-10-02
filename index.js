c3bac3f65690ba34b16a7fccf8870e193a9705f7
Anne Muriuki <muriukianne05@gmail.com>
2024-10-02 10:39:19 +0300

    add objects

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
-// Write your solution in this file!
+function updateEmployeeWithKeyAndValue(employee, key , value){
+    return{
+        ...employee,
+        [key]:value
+    }
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue (employee, key , value){
+    employee[key] = value
+    return employee
+}
+
+function deleteFromEmployeeByKey (employee,key) {
+    let newEmployee = {...employee}
+    delete newEmployee [key]
+    return newEmployee
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee , key){
+    delete employee[key]
+    return employee
+}
+
+let employee = {
+name : "Sam" , 
+streetAddress : "sdft11",
+age : 31
+}
+console.log(employee)
+
+let updateEmployee = UpdateEmployeeWithKeyAndValue (employee,"streetAddress", "sdft12")
+console.log (updatedEmployee)
+console.log(employee)
+
+destructivelyUpdateEmployeeWithKeyAndValue(employee,"age",32)
+console.log (employee)
+
+
+let newEmployee = deleteFromEmployeeByKey(employee , "age")
+console.log(newEmployee)
+console.log(employee)
+
+destructivelyDeleteFromEmployeeByKey(employee , "streetAddress")
+console.log(employee)
